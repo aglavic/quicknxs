1b13b251789a89115467a7278bd636351052e008
Artur Glavic <glavic@poweratz.(none)>
2013-02-09 16:04:39 -0500

    Add images to repository.

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 5, 15)
-last_changes="2013-02-09 15:49"
+version=(0, 5, 16)
+last_changes="2013-02-09 16:04"
 
 str_version=".".join(map(str, version))
