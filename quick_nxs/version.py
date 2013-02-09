bcb8797872faf03eabfb7e80978f7d5c77973a9d
Artur Glavic <glavic@poweratz.(none)>
2013-02-09 15:42:49 -0500

    Remove unused autoreduction dialog

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 5, 13)
-last_changes="2013-02-09 15:09"
+version=(0, 5, 14)
+last_changes="2013-02-09 15:42"
 
 str_version=".".join(map(str, version))
