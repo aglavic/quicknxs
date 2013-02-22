c62f880e8a4aeb724120b80537ec6109e96fd7d9
agf <agf@agf-laptop>
2013-02-22 14:47:15 -0500

    minor

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 5, 18)
-last_changes="2013-02-11 09:19"
+version=(0, 5, 19)
+last_changes="2013-02-22 14:45"
 
 str_version=".".join(map(str, version))
