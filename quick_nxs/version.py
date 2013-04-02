38864400be0edbc1cb28139d9567f559f06c6b18
Artur Glavic <glavica@ornl.gov>
2013-04-02 16:59:58 -0400

    Update default window settings

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 5, 28)
-last_changes="2013-04-02 16:56"
+version=(0, 5, 29)
+last_changes="2013-04-02 16:59"
 
 str_version=".".join(map(str, version))
