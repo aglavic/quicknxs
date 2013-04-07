-Fix off-specular scaling of background with sin(theta) -Correction of off-specular data for detector tails working -Don't start gui logging when ipython is set

quick_nxs/mreduce.py
@@ -911,8 +911,7 @@ class Reflectivity(object):
     reg=map(lambda item: int(round(item)),
             [bg_pos-bg_width/2., bg_pos+bg_width/2.+1,
              y_pos-y_width/2., y_pos+y_width/2.+1 ])
-    debug('Reflectivity region: %s'%str(reg))
-    debug("Background scale is %s"%(scale))
+    debug('Background region: %s'%str(reg))
 
 
     if bg_poly:
@@ -940,6 +939,7 @@ class Reflectivity(object):
       scaling_data=points_in_region.sum(axis=1)*float(reg[3]-reg[2])
       self.BGraw=unscaled_bgdata/scaling_data*scale
       self.dBGraw=sqrt(unscaled_bgdata)/scaling_data*scale
+      debug("Background scale is %s"%(scale/scaling_data))
     else:
       # restrict the intensity and background data to the given regions
       bgdata=data[reg[0]:reg[1], reg[2]:reg[3], :]
@@ -949,6 +949,7 @@ class Reflectivity(object):
       self.BGraw=bgdata.sum(axis=0).sum(axis=0)
       self.dBGraw=sqrt(self.BGraw)/size_BG*scale
       self.BGraw/=size_BG/scale
+      debug("Background scale is %s"%(scale/size_BG))
     if self.options['bg_tof_constant'] and self.options['normalization']:
       norm=self.options['normalization'].R
       reg=(self.dBGraw>0)&(norm>0)
@@ -1002,6 +1003,7 @@ class OffSpecular(Reflectivity):
     if self.options['dpix'] is None:
       self.options['dpix']=dataset.dpix
     self.lambda_center=dataset.lambda_center
+    self.options['scale_by_beam']=False
 
     self._calc_offspec(dataset)
 
@@ -1039,6 +1041,7 @@ class OffSpecular(Reflectivity):
     reg=map(lambda item: int(round(item)),
             [x_pos-x_width/2., x_pos+x_width/2.+1,
              y_pos-y_width/2., y_pos+y_width/2.+1])
+    debug('Off-Specular region: %s'%str(reg))
 
     rad_per_pixel=dataset.det_size_x/dataset.dist_sam_det/dataset.xydata.shape[1]
     xtth=self.options['dpix']-arange(data.shape[0])[DETECTOR_X_REGION[0]:DETECTOR_X_REGION[1]]
@@ -1047,6 +1050,7 @@ class OffSpecular(Reflectivity):
     af=self.options['tth']*pi/180.+xtth*rad_per_pixel-tth_spec/2.
     ai=ones_like(af)*tth_spec/2.
     self.ai=tth_spec/2.
+    debug('alpha_i=%s'%self.ai)
 
     self._calc_bg(dataset)
 
@@ -1071,6 +1075,9 @@ class OffSpecular(Reflectivity):
     self.Iraw=Idata.sum(axis=1)
     self.dIraw=sqrt(self.Iraw)
     # normalize data by width in y and multiply scaling factor
+    debug("Intensity scale is %s*%s=%s"%(scale/(reg[3]-reg[2]),
+                                        self.options['scale'],
+                                        self.options['scale']*scale/(reg[3]-reg[2])))
     self.I=self.Iraw/(reg[3]-reg[2])*scale
     self.dI=self.dIraw/(reg[3]-reg[2])*scale
     self.S=self.I-self.BG[newaxis, :]
@@ -1080,6 +1087,7 @@ class OffSpecular(Reflectivity):
 
     if self.options['normalization']:
       norm=self.options['normalization']
+      debug("Performing normalization from %s"%norm)
       idxs=norm.Rraw>0.
       self.dS[:, idxs]=sqrt(
                    (self.dS[:, idxs]/norm.Rraw[idxs][newaxis, :])**2+

quick_nxs/mrio.py
@@ -15,7 +15,7 @@ from zipfile import ZipFile
 from cPickle import loads, dumps
 from .decorators import log_call
 from .mreduce import NXSData, NXSMultiData, Reflectivity, OffSpecular
-from .mrcalc import smooth_data
+from .mrcalc import smooth_data, DetectorTailCorrector
 from .version import str_version
 from .output_templates import *
 
@@ -382,7 +382,7 @@ class Exporter(object):
   @log_call
   def extract_offspecular(self):
     '''
-      Extract the oof-specular scattering for all datasets.
+      Extract the off-specular scattering for all datasets.
     '''
     output_data=dict([(channel, []) for channel in self.channels])
     output_data['column_units']=[u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', 'a.u.', 'a.u.']
@@ -408,6 +408,47 @@ class Exporter(object):
         ki_max=max(ki_max, ki_z.max())
     output_data['ki_max']=ki_max
     self.output_data['OffSpec']=output_data
+
+  @log_call
+  def extract_offspecular_corr(self):
+    '''
+      Extract the off-specular scattering for all datasets and correct it
+      for the tails produced by the detector in x-direction.
+    '''
+    output_data=dict([(channel, []) for channel in self.channels])
+    output_data['column_units']=[u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', u'Å⁻¹', 'a.u.', 'a.u.']
+    output_data['column_names']=['Qx', 'Qz', 'ki_z', 'kf_z', 'ki_z-kf_z', 'I', 'dI']
+
+    corr_ds=self.norms[0]
+    if type(corr_ds.origin) is list:
+      flist=[origin[0] for origin in corr_ds.origin]
+      corr_data=NXSMultiData(flist, **corr_ds.read_options)[0]
+    else:
+      corr_data=NXSData(corr_ds.origin[0], **corr_ds.read_options)[0]
+    debug('Correction from normalization '+repr(corr_data))
+    corrector=DetectorTailCorrector(corr_data.xdata, x0=corr_ds.options['x_pos'])
+
+    ki_max=0.01
+    for refli in self.refls:
+      opts=refli.options
+      index=opts['number']
+      fdata=self.raw_data[index]
+      P0=len(fdata[channel].tof)-opts['P0']
+      PN=opts['PN']
+      for channel in self.channels:
+        offspec=OffSpecular(fdata[channel], **opts)
+        Qx, Qz, ki_z, kf_z, S, dS=(offspec.Qx, offspec.Qz, offspec.ki_z, offspec.kf_z,
+                                   offspec.S, offspec.dS)
+        debug('sum(S) before '+repr(S.sum()))
+        S=corrector(S)
+        debug('sum(S) after '+repr(S.sum()))
+        rdata=np.array([Qx[:, PN:P0], Qz[:, PN:P0], ki_z[:, PN:P0], kf_z[:, PN:P0],
+                      ki_z[:, PN:P0]-kf_z[:, PN:P0], S[:, PN:P0], dS[:, PN:P0]],
+                    copy=False).transpose((1, 2, 0))
+        output_data[channel].append(rdata)
+        ki_max=max(ki_max, ki_z.max())
+    output_data['ki_max']=ki_max
+    self.output_data['OffSpecCorr']=output_data
 
   @log_call
   def smooth_offspec(self, settings, pb=None):
@@ -617,7 +658,7 @@ class Exporter(object):
                   )
       params['pix_x']=1000*cols
       params['pix_y']=200+1200*rows
-      if title=='OffSpec':
+      if title in ['OffSpec', 'OffSpecCorr']:
         params['ratio']=1.5
         params['ylabel']=u"Q_z [Å^{-1}]"
         params['xlabel']=u"k_{i,z}-k_{f,z} [Å^{-1}]"