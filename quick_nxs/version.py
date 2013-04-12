f492f8291b444af021f68733d8dd47fe77052439
Artur Glavic <glavica@ornl.gov>
2013-04-12 17:25:25 -0400

    minor

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 41)
-last_changes="2013-04-12 17:19"
+version=(0, 6, 42)
+last_changes="2013-04-12 17:25"
 
 str_version=".".join(map(str, version))
