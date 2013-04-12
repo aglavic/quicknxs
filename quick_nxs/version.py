e0f47d69d0aee96de0f705ca1e41cabe1942ec91
Artur Glavic <glavica@ornl.gov>
2013-04-12 15:21:22 -0400

    travis update 5

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 25)
-last_changes="2013-04-12 15:13"
+version=(0, 6, 26)
+last_changes="2013-04-12 15:21"
 
 str_version=".".join(map(str, version))
