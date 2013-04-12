49acaef6f0c59ebb295188cb50dc849f0311a243
Artur Glavic <glavica@ornl.gov>
2013-04-12 16:52:07 -0400

    Try fixing error on 32bit machine for array view with columns of floats

--- quick_nxs/mrio.py
+++ quick_nxs/mrio.py
@@ -551,8 +551,8 @@ class Exporter(object):
     for channel in self.channels:
       d=np.vstack(output_data[channel])
       # sort dataset for alpha i and Qz
-      order=np.argsort(d.view([('Qz', float), ('R', float),
-                            ('dR', float), ('dQz', float), ('ai', float)
+      order=np.argsort(d.view([('Qz', d.dtype), ('R', d.dtype),
+                            ('dR', d.dtype), ('dQz', d.dtype), ('ai', d.dtype)
                             ]), order=['Qz', 'ai'], axis=0)
       d=d[order.flatten(), :]
 
