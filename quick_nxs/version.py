d3686821e13d455bc065a7cfafb3a094727c3c48
Artur Glavic <glavica@ornl.gov>
2013-04-12 15:13:36 -0400

    test no scipy

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 24)
-last_changes="2013-04-12 15:11"
+version=(0, 6, 25)
+last_changes="2013-04-12 15:13"
 
 str_version=".".join(map(str, version))
