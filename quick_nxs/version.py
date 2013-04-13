2561b722fa39639bbbca7a152d15faa24ee77f86
Artur Glavic <glavic@poweratz>
2013-04-12 21:54:29 -0400

    Remove gui tests for python 3 from travis as it hangs somehow

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 50)
-last_changes="2013-04-12 21:34"
+version=(0, 6, 51)
+last_changes="2013-04-12 21:54"
 
 str_version=".".join(map(str, version))
