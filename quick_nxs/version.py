ef3feb6b486080930fb82b3aad794721003c49e6
Artur Glavic <glavic@poweratz>
2013-04-12 21:34:41 -0400

    test

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 49)
-last_changes="2013-04-12 21:21"
+version=(0, 6, 50)
+last_changes="2013-04-12 21:34"
 
 str_version=".".join(map(str, version))
