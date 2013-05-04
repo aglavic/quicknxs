a8ef626a07db8dbb931cbb3400e44bd3ae8abab1
Artur Glavic <glavic@poweratz>
2013-05-04 00:25:18 -0400

    Update before importing tests

--- setup.py
+++ setup.py
@@ -87,6 +87,9 @@ if 'install' not in sys.argv:
     os.remove('MANIFEST')
 
 if 'bdist' in sys.argv:
+  # make sure revision is correct before building
+  from subprocess import call
+  call(['/usr/bin/env', 'python', 'dist_data/update_version.py'])
   print "Running unit test before compiling build distribution."
   from test_all import test_suites
   import unittest
@@ -96,9 +99,6 @@ if 'bdist' in sys.argv:
   if len(result.errors+result.failures):
     print "Not all tests were successfull, stop building distribution!"
     exit()
-  # make sure revision is correct before building
-  from subprocess import call
-  call(['/usr/bin/env', 'python', 'dist_data/update_version.py'])
 
 from quick_nxs.version import str_version
 __version__=str_version
