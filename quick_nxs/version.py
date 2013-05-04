4f54733f721ee9ba638d316ba9afe2aa65ac88d6
Artur Glavic <glavic@poweratz>
2013-05-03 23:52:38 -0400

    Keep revision number updated with the number of git commits

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 6, 72)
-last_changes="2013-05-03 18:38"
+version=(0, 6, 134)
+last_changes="2013-05-03 23:52"
 
 str_version=".".join(map(str, version))
