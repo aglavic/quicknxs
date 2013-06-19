29a7aa867869540300221d7907a5dac10ffe5799
Artur Glavic <glavica@ornl.gov>
2013-06-19 09:01:47 -0400

    Only master branch for Travis build

--- quick_nxs/version.py
+++ quick_nxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 8, 0)
-last_changes="2013-06-18 17:00:00"
+version=(0, 8, 1)
+last_changes="2013-06-19 09:01"
 
 str_version=".".join(map(str, version))
