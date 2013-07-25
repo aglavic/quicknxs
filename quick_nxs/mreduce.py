fade2b45f17ece2524dfca10ecfc79cbb2272c0f
Artur Glavic <glavica@ornl.gov>
2013-07-25 16:40:27 -0400

    Try to correct python3 byte string error

--- quick_nxs/mreduce.py
+++ quick_nxs/mreduce.py
@@ -705,17 +705,17 @@ def time_from_header(filename, nxs=None):
   stime=1.e30
   etime=0.
   for item in nxs.values():
-    if '.' in item['start_time'].value[0]:
-      start_str, start_sub=item['start_time'].value[0].split('.', 1)
+    if '.' in str(item['start_time'].value[0]):
+      start_str, start_sub=str(item['start_time'].value[0]).split('.', 1)
       start_sub=start_sub.split('-')[0]
       start_time=mktime(strptime(start_str, '%Y-%m-%dT%H:%M:%S'))+float('.'+start_sub)
-      end_str, end_sub=item['end_time'].value[0].split('.', 1)
+      end_str, end_sub=str(item['end_time'].value[0]).split('.', 1)
       end_sub=start_sub.split('-')[0]
       end_time=mktime(strptime(end_str, '%Y-%m-%dT%H:%M:%S'))+float('.'+end_sub)
     else:
-      start_str, start_sub=item['start_time'].value[0].rsplit('-', 1)
+      start_str, start_sub=str(item['start_time'].value[0]).rsplit('-', 1)
       start_time=mktime(strptime(start_str, '%Y-%m-%dT%H:%M:%S'))
-      end_str, end_sub=item['end_time'].value[0].rsplit('-', 1)
+      end_str, end_sub=str(item['end_time'].value[0]).rsplit('-', 1)
       end_time=mktime(strptime(end_str, '%Y-%m-%dT%H:%M:%S'))
     stime=min(stime, start_time)
     etime=max(etime, end_time)
