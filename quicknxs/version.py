3ab14662730b3d0e6766ea4987ef10bc69b90029
Artur Glavic <glavica@ornl.gov>
2013-09-19 18:02:30 -0400

    Update version for parallelization testing

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 8, 16)
-last_changes="2013-09-03 17:26:36"
+version=(0, 9, 'beta1')
+last_changes="2013-09-13 18:00:00"
 
 str_version=".".join(map(str, version))
