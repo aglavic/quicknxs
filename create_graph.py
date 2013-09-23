f4b335322dd8ce771253af649749e14e67d9245e
Artur Glavic <glavica@ornl.gov>
2013-09-23 16:35:20 -0400

    Update import graph appearance

--- create_graph.py
+++ create_graph.py
@@ -20,22 +20,23 @@ for line in dot.splitlines():
         'output_templates' in line or 'os' in line or 'sys' in line:
     continue
   elif line.startswith('"quicknxs/'):
-    line=line.replace('style=filled', 'style=filled, color="#00aa00" shape=box, fontsize=12').replace('quicknxs/', '')
+    line=line.replace('style=filled', 'style=filled, color="#00aa00" shape=box, fontsize=24').replace('quicknxs/', '')
   if 'PyQt4' in line or 'email' in line or 'scipy' in line or 'matplotlib/' in line or \
       'IPython' in line:
     line=line.split('/')[0]+'";'
     if first_add:
       first_add=False
-      out_dot.append('"PyQt4" [style=filled, color="#aa0000", fontsize=20];')
-      out_dot.append('"scipy" [style=filled, color="#aaaaff", fontsize=12];')
-      out_dot.append('"numpy" [style=filled, color="#0000aa", fontsize=20, fontcolor=white];')
-      out_dot.append('"matplotlib" [style=filled, color="#aa00aa", fontsize=20];')
-      out_dot.append('"IPython" [style=filled, color="#aaaa00"];')
-      out_dot.append('"h5py" [style=filled, color="#aaffaa", fontsize=20];')
+      out_dot.append('"PyQt4" [style=filled, color="#aa0000", fontsize=36];')
+      out_dot.append('"scipy" [style=filled, color="#aaaaff", fontsize=30];')
+      out_dot.append('"numpy" [style=filled, color="#0000aa", fontsize=32, fontcolor=white];')
+      out_dot.append('"matplotlib" [style=filled, color="#aa00aa", fontsize=32];')
+      out_dot.append('"IPython" [style=filled, color="#aaaa00", fontsize=24];')
+      out_dot.append('"h5py" [style=filled, color="#aaffaa", fontsize=32];')
       for sys_module in ['glob', 'subprocess', 'multiprocessing', 'traceback',
                          'zipfile', 'StringIO', 'cPickle', 'atexit', 'tempfile',
-                         'getpass', 'time', 'copy', 'inspect', 'email', 'smtplib']:
-        out_dot.append('"%s" [shape=hexagon];'%sys_module)
+                         'getpass', 'time', 'copy', 'inspect', 'email', 'smtplib',
+                         'ConfigParser', 'errno']:
+        out_dot.append('"%s" [shape=hexagon, fontsize=18];'%sys_module)
   line=line.replace('pylab', 'matplotlib').replace('cStringIO', 'StringIO')
   out_dot.append(line.replace('.py', ''))
 
