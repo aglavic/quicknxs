8302f449c0a457e74aad6508eb4f0d64c0081989
Artur Glavic <glavica@ornl.gov>
2013-10-11 17:32:14 -0400

    Create windows installer and state version 0.9

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 8, 38)
-last_changes="2013-10-11 16:06:57"
+version=(0, 9, 0)
+last_changes="2013-10-11 17:31:31"
 
 str_version=".".join(map(str, version))
