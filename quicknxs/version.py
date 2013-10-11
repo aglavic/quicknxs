7c66969553f9bc1975e9b68247163e10cefddf45
Artur Glavic <glavica@ornl.gov>
2013-10-11 17:41:16 -0400

    Fix update version for first tagged commit

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -2,6 +2,6 @@
 
 # the minor version and change date gets automatically updated on a git commit
 version=(0, 9, 0)
-last_changes="2013-10-11 17:31:31"
+last_changes="2013-10-11 17:32:14"
 
 str_version=".".join(map(str, version))
