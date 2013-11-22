74ec45e6d4a8e4df57ffcd016a17d92f18df5754
Artur Glavic <glavica@ornl.gov>
2013-11-22 16:43:55 -0500

    fix typo

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 9, 22)
-last_changes="2013-11-22 16:31"
+version=(0, 9, 23)
+last_changes="2013-11-22 16:43"
 
 str_version=".".join(map(str, version))
