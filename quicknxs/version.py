d0b3f3f08488d8b91220e6092cbb7ad20af30161
Artur Glavic <glavica@ornl.gov>
2013-11-22 16:31:32 -0500

    Fix python 3.x test in trafis yml file

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 9, 21)
-last_changes="2013-11-22 14:53:46"
+version=(0, 9, 22)
+last_changes="2013-11-22 16:31"
 
 str_version=".".join(map(str, version))
