1416e303c49d32507bcec00882bc5f91e94b99cc
Artur Glavic <glavica@ornl.gov>
2014-03-13 13:22:56 -0400

    Fix py2exe

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 9, 31)
-last_changes="2014-01-16 15:44:52"
+version=(0, 9, 34)
+last_changes="2014-03-06 10:02:38"
 
 str_version=".".join(map(str, version))
