84db1ff69b75ddd29a6eed5cb61abbae29f7ec93
Artur Glavic <glavica@ornl.gov>
2014-10-29 16:10:21 -0400

    Update documentation and tag version 1.0

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(0, 9, 62)
-last_changes="2014-08-26 16:41:16"
+version=(1, 0, 0)
+last_changes="2014-09-04 11:42:45"
 
 str_version=".".join(map(str, version))
