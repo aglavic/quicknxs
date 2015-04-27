fff3065f6c3f4eab7833e54983b2fc5ca749cd20
Jean Bilheux <bilheuxjm@ornl.gov>
2015-04-27 12:58:55 -0400

    Changing peak/back boxes will update bigTable

--- quicknxs/check_peak_back_error_widgets.py
+++ quicknxs/check_peak_back_error_widgets.py
@@ -1,10 +1,15 @@
+import colors
+from PyQt4 import QtGui
+
 class CheckErrorWidgets(object):
 	
 	def __init__(cls, self):
 		
 		[row, col] = self.getCurrentRowColumnSelected()
+		bError = False
 		if col == 0: #data
-	      
+			
+			col_index = 0
 			self.ui.data_peak1_error.setVisible(False)
 			self.ui.data_peak2_error.setVisible(False)
 			self.ui.data_back1_error.setVisible(False)
@@ -14,9 +19,7 @@ class CheckErrorWidgets(object):
 			peak1 = self.ui.dataPeakFromValue.value()
 			back1 = self.ui.dataBackFromValue.value()
 			back2 = self.ui.dataBackToValue.value()
-			
-			bError = False
-			
+						
 			if back1 > peak1:
 				bError = True
 				self.ui.data_peak1_error.setVisible(True)
@@ -28,9 +31,10 @@ class CheckErrorWidgets(object):
 				self.ui.data_back2_error.setVisible(True)
 		      
 			self.ui.data_selection_error_label.setVisible(bError)
-		    
+						
 		else: #norm
 		    
+			col_index = 6
 			self.ui.norm_peak1_error.setVisible(False)
 			self.ui.norm_peak2_error.setVisible(False)
 			self.ui.norm_back1_error.setVisible(False)
@@ -40,8 +44,6 @@ class CheckErrorWidgets(object):
 			peak1 = self.ui.normPeakFromValue.value()
 			back1 = self.ui.normBackFromValue.value()
 			back2 = self.ui.normBackToValue.value()
-			
-			bError = False
 			
 			if back1 > peak1:
 				bError = True
@@ -54,3 +56,14 @@ class CheckErrorWidgets(object):
 				self.ui.norm_back2_error.setVisible(True)
 	      
 			self.ui.norm_selection_error_label.setVisible(bError)  
+
+		_item = self.ui.reductionTable.item(row,col_index)
+		if bError:
+			_brush_BAD = QtGui.QBrush()
+			_brush_BAD.setColor(colors.VALUE_BAD)
+			_item.setForeground(_brush_BAD)
+		else:
+			_brush_OK = QtGui.QBrush()
+			_brush_OK.setColor(colors.VALUE_OK)			
+			_item.setForeground(_brush_OK)
+		self.ui.reductionTable.setItem(row,col_index,_item)
