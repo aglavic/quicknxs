b27f561904977ef4e262e89b9c3621b8ba5a052d
JeanBilheux <bilheuxjm@ornl.gov>
2015-07-21 10:30:28 -0400

    Changed version

--- quicknxsl/version.py
+++ quicknxsl/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(2, 0, 577)
+version=(1, 0, 0)
 last_changes="2015-04-29 12:51:36"
 
 str_version=".".join(map(str, version))
