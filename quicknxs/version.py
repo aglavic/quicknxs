4b6c0f9a0fcbf6c0eb696a7c00d9d7a1b7aa6eec
Doucet, Mathieu <doucetm@ornl.gov>
2017-10-31 09:03:46 -0400

    up version

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 # the minor version and change date gets automatically updated on a git commit
-version=(1, 1, 1)
+version=(1, 1, 2)
 last_changes="2017-10-31 08:49:44"
 
 str_version=".".join(map(str, version))
